b53512c21c576e20f14c92dfec39403998059abb
mdsaiyedul <mdsaiyedulmorsalin.code@gmail.com>
2023-09-01 05:54:41 +0600

    day-3

---
+++ output.js
@@ -0,0 +1,11 @@
+/*
+    JavaScript Display Possibilities
+
+    Writing into an HTML element, using innerHTML.
+    Writing into the HTML output using document.write().
+    Writing into an alert box, using window.alert().
+    Writing into the browser console, using console.log().
+*/
+
+
+document.getElementById('p1').innerHTML = "This is a test!"
